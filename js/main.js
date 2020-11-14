164fd9c2eda575439037cad32c249c3dd6d1a40c
Evgenii <jenek0907@mail.ru>
2020-11-14 15:57:31 +0300

    made hidden cards

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-function camelize(str) {
-    return str
-      .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-      .map(
-        // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-        // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-      )
-      .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
-  }
-    console.log(camelize("background-color"));
+const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+
+
+sidebarToggleBtn.onclick = function() {
+  menuIcon.classList.toggle('menu-icon-active');
+  sidebar.classList.toggle('sidebar--mobile-active');
+}
+
+const btnShowMoreCards = document.querySelector('.btn-more');
+const hiddenCards = document.querySelectorAll('.card-link--hidden');
+btnShowMoreCards.addEventListener('click', function(){
+hiddenCards.forEach( function (card) {
+  card.classList.remove('card-link--hidden');
+})
+})
